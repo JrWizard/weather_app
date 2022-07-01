9d3a6dd939547a325e4147fdb38cf13f0ac99028
Marek Sorokin <mareksorokin@gmail.com>
2022-07-01 18:07:39 +0200

    Initial Commit

---
+++ weather_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weather_app
+//
+//  Created by Marek Sorokin on 01.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weather_app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather_app
+//
+//  Created by Marek Sorokin on 01.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
